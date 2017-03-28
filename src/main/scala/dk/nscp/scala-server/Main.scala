22e407e633c0fbd093219f494b6e4ab15a0fe111
Niels Pedersen <np@reportsoft.dk>
2017-03-28 10:43:11 +0200

    Add /hello entry point (crash all others)

--- src/main/scala/dk/nscp/scala-server/Main.scala
+++ src/main/scala/dk/nscp/scala-server/Main.scala
@@ -2,7 +2,7 @@ package dk.nscp.scala_server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, HttpMethods, Uri}
 import akka.stream.ActorMaterializer
 
 import scala.concurrent.Future
@@ -15,8 +15,12 @@ object Main extends App {
   import system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  def handler(request: HttpRequest): Future[HttpResponse] =
-    Future.successful(HttpResponse(entity = "Hello, world!"))
+  import akka.http.scaladsl.server.Directives._
+
+  def handler(request: HttpRequest): Future[HttpResponse] = request match {
+    case HttpRequest(HttpMethods.GET, Uri.Path("/hello"), _, _, _) =>
+      Future.successful(HttpResponse(entity = "Hello, world!"))
+  }
 
   Http().bindAndHandleAsync(handler, "localhost", 8080)
     .onComplete {
